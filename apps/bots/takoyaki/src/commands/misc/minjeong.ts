0c1679403ca7e85a6b82e028c565e2ce23d2ffa9
Nelson Kim <nelson.kim.1994@gmail.com>
2026-07-21 13:00:42 -0400

    feat(tenor): updated Tenor to Klipy

--- apps/bots/takoyaki/src/commands/misc/minjeong.ts
+++ apps/bots/takoyaki/src/commands/misc/minjeong.ts
@@ -11,12 +11,14 @@ export const command = {
   execute: async (interaction) => {
 
     const response = await fetch(
-      `https://tenor.googleapis.com/v2/search?key=${process.env.TENOR_KEY}&contentfilter=medium&random=true&q=winter%20aespa`
+      `https://api.klipy.com/v2/search?key=${process.env.KLIPY_KEY}&contentfilter=medium&random=true&q=winter%20aespa`
     );
     const gifResponse = await response.json();
 
+        const gif = gifResponse?.results?.[0];
+
     await interaction.reply({
-      content: gifResponse.results[1].url,
+      content: gif?.url || 'No gif found',
     });
     return;
   },
